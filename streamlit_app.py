e7e4a84710894c73ff1063a15b4d220c7e757952
waryta <34441577+waryta@users.noreply.github.com>
2025-12-09 11:26:18 +0100

    Update streamlit_app.py
    connection changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,24 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
-
+#from snowflake.snowpark.context import get_active_session
 
 # Write directly to the app
-st.title(":cup_with_straw: Pending Smothies Orders:cup_with_straw:")
+st.title(":apple: Uncle Yer's Fruits Details :apple:")
 st.write(
-  """Orders that need to be filled.
+  """Enter fruit name and root depth code below.
   """
 )
 
-session = get_active_session()
-my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
-#st.dataframe(data=my_dataframe, use_container_width=True)
-if my_dataframe:
-    editable_df = st.data_editor(my_dataframe)
-    submitted = st.button('Submit')
-    
-    if submitted:
-        og_dataset =session.table("smothies.pulblic.orders")
-        edited_dataset = session.create_dataframe(editable_df)
-        try:
-            og_dataset.merge(edited_dataset
-                                ,[og_dataset['order_uid']==edited_dataset['order_uid'])
-                                ,[when_matched().update({'ORDER_FILLED':edited_dataset['order_uid']})]
-                            )
-            
-            st.success("Someone clicked the button.", icon ='👍')
-        except:
-            st.write['something went wrong']
+# Get the current credentials
+cnx = st.connection("snowflake")
+session = cnx.session()
 
-    else:
-    st.success("There are no pending request right now.", icon ='👍')
+fn = st.text_input('Fruit Name:')
+rdc = st.selectbox('Root_Deph:',('S','M','D'))
+if st.button('Submit'):
+    #st.write('Fruit Name entered is ' + fn)
+    #st.write('Root Depth Code chosen is ' + rdc)
+    sql_insert = 'insert into garden_plants.fruits.fruit_details select \''+fn+'\',\''+rdc+'\''
+    #st.write(sql_insert)
+    result=session.sql(sql_insert)
+    st.write(result)
